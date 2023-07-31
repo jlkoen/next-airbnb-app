67eb9ffd5dd3b84f773561a564a37e52615cb150
John Koen <jkoen@smartronix.com>
2023-07-31 15:16:29 -0400

    Initial navbar with logo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,26 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Nunito } from 'next/font/google'
+import './globals.css';
+import type { Metadata } from 'next';
+import { Nunito } from 'next/font/google';
+import Navbar from './components/navbar/Navbar';
 
-const inter = Nunito({ subsets: ['latin'] })
+const inter = Nunito({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'Airbnb',
   description: 'Airbnb Clone',
-}
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang='en'>
+      <body className={inter.className}>
+        <Navbar />
+        {children}
+      </body>
     </html>
-  )
+  );
 }
