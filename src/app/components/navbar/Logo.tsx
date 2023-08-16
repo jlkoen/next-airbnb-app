63c3c3f19e015532516f8f19e9ef83e6dc98580f
John Koen <jkoen@smartronix.com>
2023-08-16 15:28:24 -0400

    Completed category nav/router config

--- src/app/components/navbar/Logo.tsx
+++ src/app/components/navbar/Logo.tsx
@@ -7,11 +7,12 @@ const Logo = () => {
   const router = useRouter();
   return (
     <Image
-      className="hidden md:block cursor-pointer"
-      src="/images/logo.png"
-      height="100"
-      width="100"
-      alt="Logo"
+      onClick={() => router.push('/')}
+      className='hidden md:block cursor-pointer'
+      src='/images/logo.png'
+      height='100'
+      width='100'
+      alt='Logo'
     />
   );
 };
