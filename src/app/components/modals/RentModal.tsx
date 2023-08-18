3e80b9718f410885100e72ba32575208863f9992
John Koen <jkoen@smartronix.com>
2023-08-18 15:15:09 -0400

    Location step with map

--- src/app/components/modals/RentModal.tsx
+++ src/app/components/modals/RentModal.tsx
@@ -2,12 +2,14 @@
 
 import { useMemo, useState } from 'react';
 import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
+import dynamic from 'next/dynamic';
 
 import useRentModal from '@/app/hooks/useRentModal';
 import Modal from './Modal';
 import Heading from '../Heading';
 import { categories } from '../navbar/Categories';
 import CategoryInput from '../inputs/CategoryInput';
+import CountrySelect from '../inputs/CountrySelect';
 
 enum STEPS {
   CATEGORY = 0,
@@ -45,6 +47,15 @@ const RentModal = () => {
   });
 
   const category = watch('category');
+  const location = watch('location');
+
+  const Map = useMemo(
+    () =>
+      dynamic(() => import('../Map'), {
+        ssr: false,
+      }),
+    [location]
+  );
 
   const setCustomValue = (id: string, value: any) => {
     setValue(id, value, {
@@ -76,7 +87,7 @@ const RentModal = () => {
     return 'Back';
   }, [step]);
 
-  const bodyContent = (
+  let bodyContent = (
     <div className='flex flex-col gap-8'>
       <Heading
         title='Which of these best describes your place?'
@@ -97,11 +108,27 @@ const RentModal = () => {
     </div>
   );
 
+  if (step === STEPS.LOCATION) {
+    bodyContent = (
+      <div className='flex flex-col gap-8'>
+        <Heading
+          title='Where is your place located?'
+          subtitle='Help guests find you'
+        />
+        <CountrySelect
+          value={location}
+          onChange={(value) => setCustomValue('location', value)}
+        />
+        <Map center={location?.latlng} />
+      </div>
+    );
+  }
+
   return (
     <Modal
       isOpen={rentModal.isOpen}
       onClose={rentModal.onClose}
-      onSubmit={rentModal.onClose}
+      onSubmit={onNext}
       actionLabel={actionLabel}
       title='Airbnb your Home'
       secondaryActionLabel={secondaryActionLabel}
