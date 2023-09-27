4eed0317a62107f50d7200045c18ed3a27f195d3
John Koen <jkoen@smartronix.com>
2023-09-27 21:39:25 +0000

    Setup Jest and initial tests of Navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 export default function Home() {
-  return <div className="text-rose-500 text-2xl">Hello Airbnb!</div>;
+  return <div className='text-rose-500 text-2xl'>Hello Airbnb!</div>;
 }
