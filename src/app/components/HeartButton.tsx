e2c6b50ab730c516174d2fdddecf7cfa2d710f44
John Koen <jkoen@smartronix.com>
2023-10-21 20:05:25 +0000

    Completed favorites logic/implementation

--- src/app/components/HeartButton.tsx
+++ src/app/components/HeartButton.tsx
@@ -2,22 +2,36 @@
 
 import { AiFillHeart, AiOutlineHeart } from 'react-icons/ai';
 
+import { SafeUser } from '@/app/types';
+import useFavorite from '../hooks/useFavorite';
+
 interface HeartButtonProps {
   listingId: string;
-  currentUser?: any | null;
+  currentUser?: SafeUser | null;
 }
 
 const HeartButton: React.FC<HeartButtonProps> = ({
   listingId,
   currentUser,
 }) => {
+  const { hasFavorited, toggleFavorite } = useFavorite({
+    listingId,
+    currentUser,
+  });
+
   return (
-    <div className='relative hover:opacity-80 transition cursor-pointer'>
+    <div
+      onClick={toggleFavorite}
+      className='relative hover:opacity-80 transition cursor-pointer'
+    >
       <AiOutlineHeart
         size={28}
         className='fill-white absolute -top-[2px] -right-[2px]'
       />
-      <AiFillHeart size={24} className='fill-rose-500' />
+      <AiFillHeart
+        size={24}
+        className={hasFavorited ? 'fill-rose-500' : 'fill-neutral-500/70'}
+      />
     </div>
   );
 };
--- src/app/components/listings/ListingCard.tsx
+++ src/app/components/listings/ListingCard.tsx
@@ -1,17 +1,48 @@
 'use client';
 
 import Image from 'next/image';
+import { useRouter } from 'next/navigation';
+import { useCallback, useMemo } from 'react';
 
 import HeartButton from '../HeartButton';
 import useCountries from '@/app/hooks/useCountries';
+import { SafeUser, SafeListing } from '@/app/types';
+import Button from '../Button';
 
 interface ListingCardProps {
-  data: any;
+  data: SafeListing;
+  onAction?: (id: string) => void;
+  disabled?: boolean;
+  actionLabel?: string;
+  actionId?: string;
+  currentUser?: SafeUser | null;
 }
 
-const ListingCard: React.FC<ListingCardProps> = ({ data }) => {
+const ListingCard: React.FC<ListingCardProps> = ({
+  data,
+  onAction,
+  disabled,
+  actionLabel,
+  actionId = '',
+  currentUser,
+}) => {
+  const router = useRouter();
   const { getByValue } = useCountries();
+
   const location = getByValue(data.locationValue);
+
+  const handleCancel = useCallback(
+    (e: React.MouseEvent<HTMLButtonElement>) => {
+      e.stopPropagation();
+
+      if (disabled) {
+        return;
+      }
+
+      onAction?.(actionId);
+    },
+    [disabled, onAction, actionId]
+  );
 
   return (
     <div className='col-span-1 cursor-pointer group'>
@@ -24,7 +55,7 @@ const ListingCard: React.FC<ListingCardProps> = ({ data }) => {
             alt='Listing'
           />
           <div className='absolute top-3 right-3'>
-            <HeartButton listingId={data.id} />
+            <HeartButton listingId={data.id} currentUser={currentUser} />
           </div>
         </div>
         <div className='font-semibold text-lg'>
@@ -33,6 +64,14 @@ const ListingCard: React.FC<ListingCardProps> = ({ data }) => {
         <div className='font-light text-neutral-500'>{data.category}</div>
         <div className='flex flex-row items-center gap-1'>
           <div className='font-semibold'>$ {data.price}</div>
+          {actionLabel && (
+            <Button
+              disabled={disabled}
+              small
+              label={actionLabel}
+              onClick={handleCancel}
+            />
+          )}
         </div>
       </div>
     </div>
